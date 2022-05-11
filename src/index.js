f3ecf05396ab6c133f9ff90cb5ee87b148d326fe
Thais F <thais_fernandes62@hotmail.com>
2022-05-11 20:29:51 -0300

    only commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from 'pages/Login';
+import Router from './routes';
 
 const theme = createTheme({
   palette: {
@@ -16,11 +16,12 @@ const theme = createTheme({
   }
 })
 
+
 ReactDOM.render(
   <React.StrictMode>
     <StylesProvider injectFirst>
       <ThemeProvider theme={theme}>
-        <Login />
+        <Router />
       </ThemeProvider>
     </StylesProvider>
   </React.StrictMode>,
